19a473a82bedfbfa16669d8d580ebb43e7fa84da
Mane <61853435+Mane-jaker@users.noreply.github.com>
2022-11-20 19:43:15 -0600

    hola
    si

--- project/carrot-state/src/App.jsx
+++ project/carrot-state/src/App.jsx
@@ -7,36 +7,27 @@ import ItemPage from './pages/ItemPage'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import UploadImg from './pages/UploadImg'
-
 import PopupItem from './components/PopupItem'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+      <>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/*' element={<ErrorPage/>}/> 
+            <Route path='/' element={<Home/>}/>
+            <Route path='/login' element={<Login/>}/> 
+            <Route path='/register' element={<Register/>}/>
+            <Route path='/agentPage' element={<AgentPage/>}/>
+            <Route path='/item' element={<ItemPage/>}/>
+            <Route path='/clients' element={<ClientsPage/>}/>
+            <Route path='/uploadImg' element={<UploadImg/>}/>
+            <Route path='/popIt' element={<PopupItem/>}/>
+          </Routes>
+        </BrowserRouter>
+      </>
   )
 }
 
