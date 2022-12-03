07088f5f3e2969bd860c922af4007a1ec6eb6a82
CarlosDCG12 <alguiencomunxdxd@gmail.com>
2022-12-02 22:42:03 -0600

    lo que acabo de hacer ahorita

--- project/carrot-state/src/app/store.jsx
+++ project/carrot-state/src/app/store.jsx
@@ -1,8 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit"
 import agentReducer from "../features/agent/AgentSlice"
+import houseReducer from "../features/house/HouseSlice"
 
 export const store = configureStore ({
     reducer: {
-        agents: agentReducer
+        agents: agentReducer,
+        houses: houseReducer
     }
 })
