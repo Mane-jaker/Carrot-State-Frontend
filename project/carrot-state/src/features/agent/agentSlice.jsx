956683fde7545c456ccd4c2b5cb482304af31c25
Mane <61853435+Mane-jaker@users.noreply.github.com>
2022-12-05 14:29:23 -0600

    va
    xd

--- project/carrot-state/src/features/agent/agentSlice.jsx
+++ project/carrot-state/src/features/agent/agentSlice.jsx
@@ -1,39 +1,37 @@
-import { createSlice } from "@reduxjs/toolkit";
+import {createSlice} from "@reduxjs/toolkit"
 
-const initialState = { 
-    id: 0,
-    name: "",
-    email: "",
-    password: "",
-    profilePicture: [],
-    numberOfSales: 0,
-    numberOfPropierties: 0,
+
+const initialState= [{
+    id: "1",
+    title: "puto",
+    description: "el puto mas puto asies",
     status: false
-};
-
+}]
 
 export const agentSlice = createSlice({
-    name: 'agents',
-    initialState: {},
-    reducers: {
-        addAgent: (state, action) => {
-            state.push(action.payload)        
+    name: "agents",
+    initialState,
+    reducers:{
+        addAgent:(state, action) =>{
+            state.push(action.payload)
         },
-
-        updateAget: (state, action) => {
-            state.put(action.payload)
+        deleteAgent:(state, action) =>{
+            const agentFound = state.find(agent => agent.id === action.payload)
+            if (agentFound){
+                state.splice(state.indexOf(agentFound), 1)
+            }
         },
+        updateAgent:(state, action) =>{
+            const {id, title, description}= action.payload
+            const foundAgent = state.find(agent => agent.id === id)
 
-        deleteAgent: (state, action) => {
-            state.delet(action.payload)
+            if(foundAgent){
+                foundAgent.title = title
+                foundAgent.description = description
+            }
         }
     }
-});
+})
 
-
-
-export const { addAgent } = agentSlice.actions;
-export const { updateAget} = agentSlice.actions;
-export const { deleteAgent } = agentSlice.actions;
-
-export default agentSlice.reducer;
+export const {addAgent, deleteAgent, updateAgent} = agentSlice.actions
+export default agentSlice.reducer
